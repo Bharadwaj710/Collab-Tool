6c38a10417a90365b51263ded3103835032402ec
bharadwaj <bharadwaj10flasmvp@gmail.com>
2026-01-06 07:39:35 +0530

    Updated_login

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,11 @@
+// src/api/axios.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-
-const api = axios.create({
-  baseURL: API_URL,
+const instance = axios.create({
+baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
-export default api;
+export default instance;
